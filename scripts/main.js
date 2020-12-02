58b2c6fcc024e2f70931f83ca67de93ccfc51614
DESKTOP-94SKCK3\Jake <jake.thewoz@gmail.com>
2020-12-02 18:16:22 +0200

    Improved api functionality, improved style choices

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ let currentCity = "Washington,dc,us";
 let units = "imperial";
 
 // Here we're declaring the HTML objects that will be assigned values at init time
-let currentTemp, currentIcon;
+let currentTitle, currentTemp, currentIcon, tempGauge, tempChanger;
 
 // Bad form, but it's a free api.
 let apiKey = "c107720496abaa4ee5aefeafc5a80d66";
@@ -16,9 +16,7 @@ let apiKey = "c107720496abaa4ee5aefeafc5a80d66";
 // When the page loads, we'll see if there's a city in local storage
 document.addEventListener("readystatechange", (event) => {
     if (document.readyState === "complete") {
-        // Check local storage for a city
         console.log('readyState = complete');
-        
         initApp();
     }
 });
@@ -29,19 +27,21 @@ const initApp = function () {
 
     if (localStorage.getItem("city") !== null) {
         currentCity = localStorage.getItem("city");
+        units = localStorage.getItem("units");
     } else {
         currentCity = "Washington,dc,us";
+        units = "imperial";
     }
-
-    updateWeather(currentCity);
 
     // Here we make js objects for our interactive items on the page
     const submitButton = document.querySelector("#submit-button");
     const searchBox = document.querySelector("#search-box");
     const saveButton = document.querySelector("#save-button");
 
+    currentTitle = document.querySelector("#current-title");
     currentTemp = document.querySelector("#current-temp");
     currentIcon = document.querySelector("#current-weather-icon");
+    tempGauge = document.querySelector("#temp-gauge");
 
     // Now we add the listener to the search button
     submitButton.addEventListener("click", () => {
@@ -51,6 +51,19 @@ const initApp = function () {
     saveButton.addEventListener("click", () => {
         saveCity();
     });
+
+    tempGauge.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log('tempchanger');
+        if (units === "imperial")
+            units = "metric";
+        else 
+            units = "imperial";
+        updateWeather(currentCity);
+    }, false);
+
+    // Here we set the weather after the page loads
+    updateWeather(currentCity);
 }
 
 // Here's our function that runs on page load,
@@ -63,16 +76,23 @@ const updateWeather = async (searchValue) => {
     console.log(weatherData);
 
     // Here we update the HTML to reflect the weather
+    currentTitle.textContent="Current Weather in " + weatherData.name;
     currentTemp.textContent=weatherData.main.temp;
     const iconString = "https://openweathermap.org/img/wn/" + weatherData.weather[0].icon + ".png";
     currentIcon.src=iconString;
+    tempGaugeCheck();
 };
 
 // Here we'll call the weather API and return the json
 const getWeatherData = async (cityName) => {
     console.log('getting weather data for ' + cityName + '...');
 
-    const apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + units + "&appid=" + apiKey;
+    let apiString;
+
+    if (cityName !== null)
+        apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + units + "&appid=" + apiKey;
+    else
+        apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=" + units + "&appid=" + apiKey;
 
     const response = await fetch(apiString);
     const jsonData = await response.json();
@@ -83,7 +103,16 @@ const getWeatherData = async (cityName) => {
     return jsonData;
 }
 
+// This function is used to change the farenheit and celsius link
+const tempGaugeCheck = function () {
+    if (units === "imperial")
+        tempGauge.innerHTML="F° | <a>C°</a>";
+    else
+        tempGauge.innerHTML="C° | <a>F°</a>";
+}
+
 // This function saves the current city into local storage
 const saveCity = function () {
     localStorage.setItem("city", currentCity);
+    localStorage.setItem("units", units);
 };
